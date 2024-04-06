55ad51bede090d2baa87c3a959a542fbe8a469f6
Sheetal Uday <sheud@eg.dk>
2024-04-06 17:13:54 +0530

    Password schema created

--- backend/model/userSchema.js
+++ backend/model/userSchema.js
@@ -1,1 +1,28 @@
 // user schema 
+const mongoose=require('mongoose')
+
+const UserSchema=new mongoose.Schema({
+    name:{
+        required:true,
+        type:String
+    },
+    dob:{
+        required:true,
+        type:Date
+    },
+    phone:{
+        required:true,
+        type:Number
+    },
+    email:{
+        required:true,
+        type:String,
+        unique:true
+    },
+    password:{
+        required:true,
+        type:String
+    }
+})
+const User=mongoose.model("user",UserSchema)
+module.exports=User
